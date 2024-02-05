39780b00e8e8f21827dcfa44a39a9b04d13b378a
Rocío Hernández <rocioflauta0709@gmail.com>
2024-02-05 17:42:42 +0300

    finally the tailwind class working

--- public/src/assets/components/Quiz.jsx
+++ public/src/assets/components/Quiz.jsx
@@ -8,10 +8,10 @@ const Quiz = () => {
     <div className='container'>
       <h1 className="text-xl m-4">{Questions[currQuestion].promt}</h1>
       <div className="flex flex-col">
-        <button className='border-2 w-10 border-blue-700 m-1 p-1 text-center rounded-full bg-blue-200'>{Questions[currQuestion].optionA}</button>
-        <button className='border-2 w-10 border-blue-700 m-1 p-1 text-center rounded-full bg-blue-200'>{Questions[currQuestion].optionB}</button>
-        <button className='border-2 w-10 border-blue-700 m-1 p-1 text-center rounded-full bg-blue-200'>{Questions[currQuestion].optionC}</button>
-        <button className='border-2 w-10 border-blue-700 m-1 p-1 text-center rounded-full bg-blue-200'>{Questions[currQuestion].optionD}</button>
+        <button className='button-start hover:bg-blue-600'>{Questions[currQuestion].optionA}</button>
+        <button className='button-start hover:bg-blue-600'>{Questions[currQuestion].optionB}</button>
+        <button className='button-start hover:bg-blue-600'>{Questions[currQuestion].optionC}</button>
+        <button className='button-start hover:bg-blue-600'>{Questions[currQuestion].optionD}</button>
       </div>
     </div>
   )
